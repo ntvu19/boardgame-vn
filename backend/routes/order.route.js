6f9395622df3d834626719e225a880e277149d56
nddat1811 <nddat1811@gmail.com>
2022-01-30 10:58:32 +0700

    view All and view Detail order

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,12 +1,13 @@
-const OrderController = require('../controllers/order.controller');
+﻿const OrderController = require('../controllers/order.controller');
 const express = require('express');
 const router = express.Router();
 
-// router.get('/view', OrderController.viewAll);
+router.get('/view', OrderController.viewAll);
+router.get('/view-detail-order/:id', OrderController.viewDetailOrder);
 
 /**
- * 1. Lấy danh sách đơn hàng
- * 2. Chi tiết đơn hàng
+ * 1. Lấy danh sách đơn hàng #
+ * 2. Chi tiết đơn hàng #
  * 3. Tạo đơn hàng
  */
 
