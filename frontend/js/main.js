b340977232c7bcfdeed82c58c3df75445023dc4b
19127049 <huongngan2001@gmail.com>
2022-02-11 19:13:12 +0700

    fix card-item

---
+++ frontend/js/main.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", function(){
+    truncateCardText();
+    truncateCardPrice();
+});
+
+function truncateCardText(){
+    var cardList = document.getElementsByClassName("card-text");
+    for(var i = 0; i < cardList.length; i++){
+        var text = cardList[i].innerHTML;
+        var newText = truncateString(text, 40);
+        cardList[i].innerHTML = newText;
+    }
+}
+
+function truncateString(str, num){
+    if (str.length > num){
+        return str.slice(0, num) + "...";
+    } else {
+        return str;
+    }
+}
+
+function truncateCardPrice(){
+    var cardList = document.getElementsByClassName("card-price-num");
+    for(var i = 0; i < cardList.length; i++){
+        var text = cardList[i].innerHTML;
+        var newText = truncatePrice(text);
+        cardList[i].innerHTML = newText;
+    }
+}
+
+function truncatePrice(str) {
+    if (str.length > 4){
+        return str.slice(0, str.length - 3) + "." + str.slice(str.length - 3,str.length);
+    }
+     else {
+        return str;
+    }
+}
