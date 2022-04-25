b93792708d7d406206dd1c8bcbefc1673206cabe
nddat1811 <nddat1811@gmail.com>
2022-04-25 16:17:30 +0700

    update detail

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -12,7 +12,7 @@ const imageUpload = multer.fields([{ name: 'image1', maxCount: 1 }, { name: 'ima
 //User
 router.get('/api/user-size',Auth.isAdmin, AdminController.getUserSize)
 router.get('/user/list/:offset',Auth.isAdmin,  AdminController.userPagination)
-router.get('/user/search',Auth.isAdmin, AdminController.userSearch);
+router.get('/user/search', AdminController.userSearch);
 
 // router.get('/customer', Auth.isAdmin, AdminController.userIndex);
 
